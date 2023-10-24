76610a7cd86ba16124669cf12592cd46f3253d5d
Enzo Sierra <enzogsierra@gmail.com>
2023-10-24 16:45:12 -0300

    Se añadieron los principales microservicios

---
+++ estudiantes/src/main/java/com/microservicios/estudiantes/EstudiantesApplication.java
@@ -0,0 +1,16 @@
+package com.microservicios.estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesApplication.class, args);
+	}
+
+}
